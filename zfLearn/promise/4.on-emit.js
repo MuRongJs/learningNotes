6e3fa513ae98604a1776516d305ebd2275a82092
murong <ccc2xx@163.com>
2020-09-08 17:53:23 +0800

    promise  高阶函数  函数柯里化  fs模块  发布-订阅模式

---
+++ zfLearn/promise/4.on-emit.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+// 订阅     发布模式
+
+
+
+
+let obj = {};
+
+function after(timer, callback){
+    return function(){
+        --timer === 0 && callback();
+    }
+}
+
+let fn = after(2, () => {
+    console.log(obj)
+});
+
+
+fs.readFile('name.txt', 'utf8', function(err, data){
+    if(err) return console.log(err);
+    obj.name = data;
+    fn();
+})
+fs.readFile('age.txt', 'utf8', function(err, data){
+    if(err) return console.log(err);
+    obj.age = data;
+    fn();
+})
